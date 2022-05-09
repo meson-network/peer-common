758d605174b6ac9590995f48aa189e4ee069f978
leo <leo@leos-MacBook-Pro.local>
2022-05-09 21:49:24 +0800

    update

--- dns/cert.go
+++ dns/cert.go
@@ -4,7 +4,7 @@ import "github.com/meson-network/peer_common/api"
 
 // @Description Msg_Resp_HeartBeat
 type Msg_Resp_Cert struct {
-	Cert string `json:"cert"`
-	Key  string `json:"key"`
+	Crt string `json:"crt"`
+	Key string `json:"key"`
 	api.API_META_STATUS
 }
