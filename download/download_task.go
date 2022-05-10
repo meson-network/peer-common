749441f4027ccee3974c7aa0b2f00529a9af75dc
leo <leo@leos-MacBook-Pro.local>
2022-05-10 21:40:55 +0800

    update

--- download/download_task.go
+++ download/download_task.go
@@ -4,8 +4,8 @@ import "github.com/meson-network/peer_common/api"
 
 const DOWNLOAD_THREADS_MAX = 10 //max number of parellel downloading threads
 
-// @Description Msg_Resp_Download
-type Msg_Resp_Download struct {
+// @Description Msg_Resp_Download_Task
+type Msg_Resp_Download_Task struct {
 	Origin_url string `json:"origin_url"`
 	Url_hash   string `json:"url_hash"`
 	api.API_META_STATUS
