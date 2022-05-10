9e7c3ea279a28b39c2697a2badb66b9a0a0dee11
leo <leo@ip-192-168-43-35.us-west-2.compute.internal>
2022-05-10 20:17:24 +0800

    update

--- download/download.go
+++ download/download.go
@@ -1,3 +1,12 @@
 package download
 
+import "github.com/meson-network/peer_common/api"
+
 const DOWNLOAD_THREADS_MAX = 10 //max number of parellel downloading threads
+
+// @Description Msg_Resp_Download
+type Msg_Resp_Download struct {
+	Origin_url string `json:"origin_url"`
+	Url_hash   string `json:"url_hash"`
+	api.API_META_STATUS
+}
