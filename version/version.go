a09442cc202a03f1a9ab834b6b70bf366f3eb8e9
goodboy3 <zhangzhengbo@hotmail.com>
2022-06-16 21:05:32 +0800

    commit

--- version/version.go
+++ version/version.go
@@ -9,4 +9,5 @@ type Msg_Resp_NodeVersion struct {
 	api.API_META_STATUS
 	Allow_version  string `json:"allow_version"`
 	Latest_version string `json:"latest_version"`
+	Download_host  string `json:"download_host"`
 }
