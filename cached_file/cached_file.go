5576eb0946eaca2de5d8552aa1cbdf45fa188b8d
goodboy3 <zhangzhengbo@hotmail.com>
2022-06-21 09:30:46 +0800

    commit

--- cached_file/cached_file.go
+++ cached_file/cached_file.go
@@ -1,14 +1,19 @@
 package cached_file
 
+import (
+	"github.com/meson-network/peer_common/api"
+)
+
 // @Description Msg_Req_FileExpire
 type Msg_Req_FileExpire struct {
 	Expired_files []string `json:"expired_files"` //required
 }
 
-//// @Description Msg_Resp_HeartBeat
-//type Msg_Resp_FileExpire struct {
-//	api.API_META_STATUS
-//}
+// @Description Msg_Resp_FileExpire
+type Msg_Resp_FileExpire struct {
+	api.API_META_STATUS
+	Keep_files []string `json:"keep_files"`
+}
 
 // @Description Msg_Req_FileExpire
 type Msg_Req_FileMissing struct {
