fb7a23dfb31df5281b1ddd9e46abba1be6aa8aec
goodboy3 <zhangzhengbo@hotmail.com>
2022-08-15 22:33:21 +0800

    commit

--- download/download_task.go
+++ download/download_task.go
@@ -4,8 +4,10 @@ const DOWNLOAD_THREADS_MAX = 10 //max number of parellel downloading threads
 
 // @Description Msg_Req_Download_Task
 type Msg_Req_Download_Task struct {
-	Origin_url string `json:"origin_url"` //required
-	File_hash  string `json:"file_hash"`  //required
+	Origin_url             string `json:"origin_url"`             //required
+	File_hash              string `json:"file_hash"`              //required
+	No_access_maintain_sec int64  `json:"no_access_maintain_sec"` //required
+	Single_file_limit_byte int64  `json:"single_file_limit_byte"` //required
 }
 
 //// @Description Msg_Resp_HeartBeat
